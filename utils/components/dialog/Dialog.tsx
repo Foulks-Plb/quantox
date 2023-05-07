d0d8870c7d19e1ecae5b611a5bab716b4b680516
Foulques Pellabeuf <foulques.pellabeuf@onbrane.com>
2023-05-07 20:58:34 +0200

    fix: test path scss

--- utils/components/dialog/Dialog.tsx
+++ utils/components/dialog/Dialog.tsx
@@ -1,4 +1,4 @@
-import styles from './Dialog.module.scss';
+import styles from 'scss/Dialog.module.scss';
 import { useState } from 'react';
 import { postCall } from '@/utils/ts/api-base';
 import SwapForm from './swap-form/Swap-Form';
